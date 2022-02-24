1c3038a4b070d58f6b306e5890ca355fc5ed2a4a
Dimitri <dimitri.debleser@heig-vd.ch>
2022-02-24 16:15:58 +0100

    constructeur matrix

--- Matrix.cpp
+++ Matrix.cpp
@@ -3,3 +3,12 @@
 //
 
 #include "Matrix.h"
+
+
+Matrix::Matrix(size_t N, size_t M, unsigned n)  : N(N), M(M), n(n) {
+   tab = new int[N * M];
+   for (size_t i = 0; i < N * M; ++i) {
+      *(tab + i) = rand() / (RAND_MAX + 1) * n;
+   }
+
+}
--- Matrix.h
+++ Matrix.h
@@ -5,8 +5,15 @@
 #ifndef POA_MATRIX_MATRIX_H
 #define POA_MATRIX_MATRIX_H
 
+#include <algorithm>
 
 class Matrix {
+   const size_t N, M;
+   const unsigned n;
+   int* tab;
+
+public:
+   Matrix(size_t N, size_t M, unsigned n);
 
 };
 
