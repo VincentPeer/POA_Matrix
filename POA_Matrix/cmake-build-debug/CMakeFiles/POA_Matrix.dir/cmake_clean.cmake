482403899d4776c21b9e2d8856170b533df4dfbf
Vincent Peer <71083912+VincentPeer@users.noreply.github.com>
2022-02-24 15:43:08 +0100

    Add files via upload

---
+++ POA_Matrix/cmake-build-debug/CMakeFiles/POA_Matrix.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POA_Matrix.dir/main.cpp.obj"
+  "POA_Matrix.exe"
+  "POA_Matrix.exe.manifest"
+  "POA_Matrix.pdb"
+  "libPOA_Matrix.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/POA_Matrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
