0d77234537d629e47db59a75f1c5838233d91aaf
Dimitri <dimitri.debleser@heig-vd.ch>
2022-02-26 18:40:27 +0100

    addition

--- Matrix.cpp
+++ Matrix.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "Matrix.h"
+#include <algorithm>
 
 using namespace std;
 
@@ -11,15 +12,74 @@ Matrix::Matrix(size_t N, size_t M, unsigned n)  : N(N), M(M), n(n) {
    for (size_t i = 0; i < N * M; ++i) {
       *(tab + i) = rand() / (RAND_MAX + 1) * n;
    }
-
 }
+
+Matrix::Matrix(const Matrix &matrix) : N(matrix.N), M(matrix.M), n(matrix.n) {
+   tab = new int[N * M];
+   memcpy(tab, matrix.tab, N * M);
+}
+
+//void *Matrix::operator new(size_t N, size_t M, unsigned int n) {
+//   Matrix tmp(N, M, n);
+//   auto* ptr = (Matrix*) malloc(sizeof(tmp));
+//   memcpy(ptr, &tmp, sizeof(tmp));
+//   return ptr;
+//}
+
 
 ostream& operator<<(ostream& lhs, const Matrix& rhs) {
 	for (size_t i = 0; i < rhs.N * rhs.M; ++i) {
-		if(i % rhs.M == 0 && i)
-			cout << endl;
-		cout << rhs.tab[i];
+		cout << rhs.tab[i++];
+      if(i % rhs.M == 0 && i < rhs.n * rhs.M)
+         cout << endl;
 	}
 
 	return lhs;
 }
+
+Matrix Matrix::operator+(const Matrix &rhs) const {
+   Matrix cpy = *this;
+   for_each(cpy, rhs,  add);
+   return cpy;
+}
+
+Matrix& Matrix::operator+=(const Matrix& rhs) {
+   for_each(*this, rhs,  add);
+   return *this;
+}
+
+Matrix* Matrix::newSum(const Matrix& a, const Matrix& b) {
+   Matrix* tmp = new Matrix(a);
+   for_each(*tmp, b,  add);
+   return tmp;
+}
+
+void Matrix::for_each(Matrix& m1, const Matrix& m2, int (*f)(int, int)) {
+   size_t size = max(m1.M * m1.N, m2.M * m2.N);
+   for (size_t i = 0; i < size; ++i) {
+      m1.tab[i] = f(m1.tab[i], m2.tab[i]);
+   }
+}
+
+
+//
+//int* Matrix::operator[](int i) {
+//   return tab + (i * M);
+//}
+
+int Matrix::multiply(int a, int b) {
+   return a + b;
+}
+
+int Matrix::sub(int a, int b) {
+   return a - b;
+}
+
+int Matrix::add(int a, int b) {
+   return a * b;
+}
+
+
+
+
+
--- Matrix.h
+++ Matrix.h
@@ -14,16 +14,25 @@ class Matrix {
    const unsigned n; // todo n -> MODULUS ? (const en majuscul)
    int* tab;
 
-	friend std::ostream& operator<<(std::ostream& lhs, const Matrix& rhs);
-	friend Matrix& operator+(const Matrix& lhs, const Matrix& rhs);
-	friend Matrix* operator+(const Matrix& lhs, const Matrix& rhs);
+   static void for_each(Matrix& m1, const Matrix& m2, int (*f)(int a, int b));
+   static int add(int a, int b);
+   static int sub(int a, int b);
+   static int multiply(int a, int b);
+//	friend Matrix operator+(const Matrix& lhs, const Matrix& rhs);
+//	friend Matrix* operator+=(const Matrix& lhs, const Matrix& rhs);
 public:
    Matrix(size_t N, size_t M, unsigned n);
    Matrix(const Matrix& matrix);
-	Matrix operator+(const Matrix& rhs);
+//   int* operator[](int i);
+   friend std::ostream& operator<<(std::ostream& lhs, const Matrix& rhs);
 
-   Matrix operator-(const Matrix& rhs);
-   Matrix operator*(const Matrix& rhs);
+//   void* operator new(size_t N, size_t M, unsigned n);
+	Matrix operator+(const Matrix& rhs) const;
+	Matrix& operator+=(const Matrix& rhs);
+	static Matrix* newSum(const Matrix& a, const Matrix& b);
+
+//   Matrix operator-(const Matrix& rhs);
+//   Matrix operator*(const Matrix& rhs);
 
 
 };
